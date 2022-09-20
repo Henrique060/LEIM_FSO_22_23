664b13667c8761a4d7efc2b6e64a3083525e3c28
Henrique060 <henrique.pereira.matos@gmail.com>
2022-09-20 16:21:45 +0100

    First

---
+++ DemoAula/src/demo/fso/consola/DemoConsola.java
@@ -0,0 +1,23 @@
+package demo.fso.consola;
+
+import robot.RobotLegoEV3;
+
+public class DemoConsola {
+
+	public static void main(String[] args) throws InterruptedException {
+		RobotLegoEV3 r = new RobotLegoEV3();
+		if(r.OpenEV3("EV3") == true) {
+			r.Reta(10);
+			r.CurvarDireita(360, 0);
+			r.Reta(-10);
+			r.Parar(false);
+			
+			Thread.sleep(30 * 1000); //fazer uma pausa de 30 segundos
+			
+			r.CloseEV3();
+		}
+		// TODO Auto-generated method stub
+
+	}
+
+}
